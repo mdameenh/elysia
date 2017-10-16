4e9f4734d83af5c957f8af7f9c969a124e989006
Ameen <mdameenh@gmail.com>
2017-10-16 16:54:54 +0530

    working player info filters

--- fplassist/generate_table.py
+++ fplassist/generate_table.py
@@ -15,6 +15,8 @@ import os
 from urllib import parse
 import psycopg2
 
+avail_template = {'a': 'Available', 'i': 'Injured', 'd': 'Doubtful', 'u': 'Unavailable', 's': 'Suspended', 'n': 'Unavailable'}
+
 def generate_table():
     conn = None
     try:        
@@ -42,7 +44,6 @@ def generate_table():
     
     cur.execute("SELECT * FROM team_info")
     team_info = cur.fetchall()
-    avail_template = {'a': 'Available', 'i': 'Injured', 'd': 'Doubtful', 'u': 'Unavailable', 's': 'Suspended', 'n': 'Unavailable',}
     for player in player_info:
         stat = [_stat for _stat in player_base_stats if _stat[0] == player[0]][0]
         team = [_team for _team in team_info if _team[0] == player[4]][0]
@@ -55,13 +56,59 @@ def generate_table():
     
     player_list = sorted(player_list, key=lambda x : x[4], reverse=True)
     cur.close()
-    return player_list[:50]
+    return player_list
 
 def update_table(data):
-    myresponse = {'Positions': data.getlist('position[]'),
-                  'Teams': data.getlist('team[]'),
-                  'Availability': data.getlist('availability[]'),
-                  'Difficulty': data.getlist('difficulty[]')}
-    print(myresponse)
-    return myresponse
+    filter_pos = data.getlist('position[]')
+    filter_team = data.getlist('team[]')
+    filter_avail = data.getlist('availability[]')
+    filter_diff = data.getlist('difficulty[]')
+    conn = None
     
+    try:        
+        parse.uses_netloc.append("postgres")
+        url = parse.urlparse(os.environ["DATABASE_URL"])
+        
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+    except Exception as e:
+        print(e)
+
+    cur = conn.cursor()
+    
+    cur.execute("SELECT * FROM player_info")
+    player_list = []
+    player_info = cur.fetchall()
+    
+    cur.execute("SELECT * FROM player_base_stats")
+    player_base_stats = cur.fetchall()
+    
+    cur.execute("SELECT * FROM team_info")
+    team_info = cur.fetchall()
+    for player in player_info:
+        stat = [_stat for _stat in player_base_stats if _stat[0] == player[0]][0]
+        team = [_team for _team in team_info if _team[0] == player[4]][0]
+
+        if team[1] not in filter_team:
+            continue
+        elif player[3] not in filter_pos:
+            continue
+        elif avail_template[player[5]] not in filter_avail:
+            continue
+        
+        print(player[1])
+        player_list.append([player[1], team[2], stat[3], player[2], stat[1], 
+                            stat[13], stat[11], stat[12], stat[10], 
+                            avail_template[player[5]], stat[8], stat[9], stat[4], stat[2]])
+    
+        #name, cost, position, points, form, fixture, t_in, t_out, bps, availability, cleansheet, saves, tsb, minutes    
+    
+    player_list = sorted(player_list, key=lambda x : x[4], reverse=True)
+    cur.close()
+    
+    return player_list
