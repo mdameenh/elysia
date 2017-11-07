c52a025a282b8a2f992cea261a443cb2a6573afd
Ameen <mdameenh@gmail.com>
2017-11-07 22:20:06 +0530

    fix for wrong import name render

--- elysia/settings.py
+++ elysia/settings.py
@@ -26,7 +26,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'elysia1.herokuapp.com', '127.0.0.1']
 
